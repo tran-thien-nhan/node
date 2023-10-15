9801dc5ef3645639846dbf52aa27edd661eb7596
tran-thien-nhan <pipclupnomad@gmail.com>
2023-10-15 12:38:15 +0700

    des

--- index.js
+++ index.js
@@ -1,45 +1,91 @@
 var express = require("express");
 var app = express();
 var port = 5000;
-var expressLayouts = require("express-ejs-layouts"); //gọi thư viện layout
+var expressLayouts = require("express-ejs-layouts"); // Gọi thư viện layout
+var nodemailer = require("nodemailer"); // Gọi thư viện nodemailer
+var crypto = require("crypto"); // Gọi thư viện crypto
 
 app.use(express.static("public"));
-app.set("view engine", "ejs"); //khai bao file co duoi mở rộng ejs
-app.set("views", "./views"); //set thư mục view
-app.use(expressLayouts); //sẽ chạy trang có tên layout
-
-app.use(express.json()); //đuôi mở rộng ejs
-app.use(express.urlencoded({ extended: true })) //thư mục views
+app.set("view engine", "ejs"); // Khai bao file co duoi mở rộng ejs
+app.set("views", "./views"); // Set thư mục view
+app.use(expressLayouts); // Sẽ chạy trang có tên layout
+app.use(express.json()); // Đuôi mở rộng ejs
+app.use(express.urlencoded({ extended: true }) // Thư mục views
+);
 app.listen(port);
 
 app.get("/", function (req, res) {
     res.render("trangchu");
 });
 
-//đường dẫn trang giới thiệu
+// Đường dẫn trang giới thiệu
 app.get("/gioi-thieu", function (req, res) {
     res.render("gioithieu");
 });
 
-// app.get("/thuat-toan-des", function (req, res) {
-//     res.render("thuattoandes")
-// });
+app.get("/des", function (req, res) {
+    res.render("partials/des.ejs");
+});
 
-// app.get("/thuat-toan-aes", function (req, res) {
-//     res.render("thuattoanaes")
-// });
+app.get("/email", function (req, res) {
+    res.render("partials/email");
+});
 
-// app.get("/thuat-toan-rsa", function (req, res) {
-//     res.render("thuattoanrsa")
-// });
+app.post("/post-email", function (req, res) {
+    // Cài đặt email
+    var option = {
+        service: 'gmail', // Dùng Gmail
+        auth: {
+            user: 'pipclupnomad@gmail.com',
+            pass: 'zuad vtqu kyap ovte'
+        }
+    };
+    var transporter = nodemailer.createTransport(option);
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Kết nối thành công");
+        }
+    });
+    var mail = {
+        from: req.body.emailgui,
+        to: req.body.emailnhan,
+        subject: req.body.chude,
+        html: req.body.noidung,
+    };
 
-// app.post("/post-thuat-toan-des", function (req, res) {
-//     //code ma hoa
-//     // ma hoa doi xung dung chung 1 key, trong vd nay dung key cp ten bien la key
-//     var crypto = require("crypto"); // goi thu vien ma hoa
-//     var key = Buffer.from("jhsgatt12sgsssqswhwqfsaxasxuasxs", "base64");
-//     var kieumahoa = crypto.createCipheriv("des-ede3", key, null);
-//     var mahoa = kieumahoa.update(req.body.dulieu, "utf8", "base64");
-//     mahoa += kieumahoa.final("base64");
-//     res.send(mahoa);
-// });
+    transporter.sendMail(mail, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
+
+    res.redirect("/email");
+});
+
+app.post("/post-thuat-toan-des", (req, res) => {
+    const inputData = req.body.dulieu;
+    const key = Buffer.from("jhsgatt12sgsssqswhwqfsaxasxuasxs", "base64");
+
+    // Mã hóa dữ liệu
+    const encrypt = crypto.createCipheriv("des-ede3", key, null);
+    let encryptedData = encrypt.update(inputData, "utf8", "base64");
+    encryptedData += encrypt.final("base64");
+
+    res.send(encryptedData);
+});
+
+app.post("/post-thuat-toan-des-decrypt", (req, res) => {
+    const inputData = req.body.dulieugiaima;
+    const key = Buffer.from("jhsgatt12sgsssqswhwqfsaxasxuasxs", "base64");
+
+    // Giải mã dữ liệu
+    const decrypt = crypto.createDecipheriv("des-ede3", key, null);
+    let decryptedData = decrypt.update(inputData, "base64", "utf8");
+    decryptedData += decrypt.final("utf8");
+
+    res.send(decryptedData);
+});
